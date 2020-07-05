d8bd700df3a26df1ff597eda4d89ca1674b30bac
John Cassidy <jpcassidy@outlook.com>
2020-07-05 11:45:40 -0400

    Adding HTTP Error Handling with RxJS

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { throwError } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -10,6 +12,21 @@ export class DataService {
   constructor(private httpClient: HttpClient) {}
 
   public sendGetRequest() {
-    return this.httpClient.get(`${this.REST_API_SERVER}/products/`);
+    return this.httpClient
+      .get(`${this.REST_API_SERVER}/products/`)
+      .pipe(catchError(this.handleError));
+  }
+
+  handleError(error: HttpErrorResponse) {
+    let errorMessage = 'Unkown error!';
+    if (error.error instanceof ErrorEvent) {
+      // client-side errors
+      errorMessage = `Error: ${error.error.message}`;
+    } else {
+      // server-side errors
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    window.alert(errorMessage);
+    return throwError(errorMessage);
   }
 }
